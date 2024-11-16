c70160efa8d43866dde561f86dd242de2e226514
sriram0620 <suryasriramamurthy2003@gmail.com>
2024-11-17 01:47:08 +0530

    sloved some errors

--- mentalhealth/settings.py
+++ mentalhealth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r&glp(tc0=7u_)fret-hrw6r4=(hv&9hmeom$43vyw6=r1*#wa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0989-175-101-105-133.ngrok-free.app']
+ALLOWED_HOSTS = ['*']
 
 
 
